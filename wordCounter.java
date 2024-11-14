7c0b65bf40facd9c2f064bf4fbba908c7cb649af
IA-42 Group 16, Andrii Babelo, Kapyrina Diana Oleksiivna, Khytruk Elizaveta Igorivna <babelo.andrii@lll.kpi.ua>
2024-11-15 00:28:32 +0200

    Lab9

--- wordCounter.java
+++ wordCounter.java
@@ -1,10 +1,10 @@
 public class wordCounter {
     public static void main(String[] args) {
 
-        String sentence1 = "The user with the nickname koala757677 this month left 3 times more comments than the user with the nickname croco181dile181920 4 months ago";
+        String sentence1 = "The user with the nickname koala757677 this month left 3 times more comments than the  user with the nickname croco181dile181920 4 months ago";
         System.out.println(countWords(sentence1));
 
-        String sentence2 = "лул фщ";
+        String sentence2 = "sds we2 ujl";
         System.out.println(countWords(sentence2));
 
         String sentence3 = "";
@@ -19,9 +19,15 @@ public class wordCounter {
         if (sentence == null) {
             throw new NullPointerException("речення має бути не нульове");
         }
-        String[] words = sentence.split("\\s+");
-        return words.length;
+        String lowercased = sentence.toLowerCase();
+        String[] words = lowercased.split("\\s+");
+        int count = 0;
+        for (String word : words) {
+        if (word.matches("[a-zA-Zа-яА-Я]+")){
+            count++;
+            }
+        }
+        return count;
     }
-
 
 }
